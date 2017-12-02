5ce144183bb3daccd311ffaff2a228addde0e0b7
ZedPea <zp+github@zacharypalmer.co.uk>
2017-12-02 16:55:38 +0000

    Improve solution 2-2

--- day02-2.hs
+++ day02-2.hs
@@ -5,14 +5,4 @@ checksum :: [[Int]] -> Int
 checksum = sum . map checksum'
 
 checksum' :: [Int] -> Int
-checksum' [] = error "No evenly divisible values in list!"
-checksum' (x:xs)
-    | (Just result) <- evenDivisor x xs = result
-    | otherwise = checksum' xs
-
-evenDivisor :: Int -> [Int] -> Maybe Int
-evenDivisor _ [] = Nothing
-evenDivisor x (y:ys)
-    | x `rem` y == 0 = Just (x `div` y)
-    | y `rem` x == 0 = Just (y `div` x)
-    | otherwise = evenDivisor x ys
+checksum' xs = head [x `div` y | x <- xs, y <- xs, x /= y, x `rem` y == 0]
