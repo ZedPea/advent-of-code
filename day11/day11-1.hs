417043bc4af6a252fcaae822e7dbd44cb070763a
ZedPea <zp+github@zacharypalmer.co.uk>
2017-12-13 21:30:23 +0000

    Add day-11

---
+++ day11/day11-1.hs
@@ -0,0 +1,33 @@
+import Data.List.Split (wordsBy)
+import Control.Arrow ((***))
+import Control.Monad (join)
+
+data Dir = NorthWest
+         | North
+         | NorthEast
+         | SouthEast
+         | South
+         | SouthWest
+
+-- skip the \n in input with init
+main :: IO ()
+main = print =<< shortestDistance . map parseDir . wordsBy (== ',') . init <$> getContents
+
+parseDir :: String -> Dir
+parseDir "nw" = NorthWest
+parseDir "n" = North
+parseDir "ne" = NorthEast
+parseDir "se" = SouthEast
+parseDir "s" = South
+parseDir "sw" = SouthWest
+
+shortestDistance :: [Dir] -> Int
+shortestDistance = round . uncurry (+) . join (***) abs . foldl adjustDistance (0, 0)
+
+adjustDistance :: (Double, Double) -> Dir -> (Double, Double)
+adjustDistance (x, y) NorthWest = (x - 0.5, y + 0.5)
+adjustDistance (x, y) North     = (x,       y + 1.0)
+adjustDistance (x, y) NorthEast = (x + 0.5, y + 0.5)
+adjustDistance (x, y) SouthEast = (x + 0.5, y - 0.5)
+adjustDistance (x, y) South     = (x,       y - 1.0)
+adjustDistance (x, y) SouthWest = (x - 0.5, y - 0.5)
